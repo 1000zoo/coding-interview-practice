3c662755366397c7a5cdb6d8088a9e55d1188339
1000zoo <cjswldn99@gmail.com>
2022-08-18 21:26:41 +0900

    prob 1.9 / 문자열 회전

--- src/chapter01_Array_and_String/Prob09.java
+++ src/chapter01_Array_and_String/Prob09.java
@@ -9,6 +9,8 @@ ex) waterbottle -> erbottlewat (true)
 
 public class Prob09 {
 
+    // Solution 1
+    // Big-O => O(N) (isSubstring() 메서드에서)
     public static boolean isRotation(String s1, String s2) {
         if (s1.length() != s2.length()) {
             return false;
@@ -17,12 +19,28 @@ public class Prob09 {
         return isSubstring(temp, s2);
     }
 
-    public static void main(String[] args) {
-        String s1 = "waterbottle";
-        String s2 = "erbottlewa";
-        System.out.println(isRotation(s1, s2));
+    public static boolean isRotation2(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        String temp1, temp2;
+        for (int i = 0; i < s2.length(); i++) {
+            temp1 = s2.substring(0, i);
+            temp2 = s2.substring(i);
+            if (isSubstring(s1, temp1) && isSubstring(s2, temp2)) {
+                return true;
+            }
+        }
+        return false;
     }
 
+    public static void main(String[] args) {
+        String s1 = "waterbottle";
+        String s2 = "erbottlwwat";
+        System.out.println(isRotation2(s1, s2));
+    }
+
+    // 한 단어가 다른 문자열에 포함되어 있는지 판별하는 메서드
     public static boolean isSubstring(String s1, String s2) {
         int len = s1.length() - s2.length() + 1;
         String temp;
