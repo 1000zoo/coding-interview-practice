570af108d2a91f94800c853e924b29b1819885b2
1000zoo <cjswldn99@gmail.com>
2022-09-14 05:00:10 +0900

    prob 2.5 / 리스트의 합

--- src/chapter02_LinkedList/Prob05.java
+++ src/chapter02_LinkedList/Prob05.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 
 public class Prob05 {
 
+    //Time : O(h1 + h2)
+    //Space: O(h1 + h2)
     public static ListNode sum(ListNode head1, ListNode head2) {
         ListNode answer = new ListNode();
         ListNode ac = answer;
