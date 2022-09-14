1d080201ef6a9d109b9a5f47bce08dc89e7c3094
1000zoo <cjswldn99@gmail.com>
2022-09-14 14:59:24 +0900

    prob 2.8 / 루프 발견

---
+++ src/chapter02_LinkedList/Prob08.java
@@ -0,0 +1,27 @@
+package chapter02_LinkedList;
+
+import java.util.HashSet;
+
+/*
+2.8 루프 발견 : 순환 연결리스트가 주어졌을 때, 순환되는 부분의 첫째 노드를 반환하는 알고리즘을 작성하라.
+ */
+
+public class Prob08 {
+
+    public static ListNode circularNode(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        ListNode curr = head;
+        while (curr != null) {
+            if (set.contains(curr)) {
+                return curr;
+            }
+            set.add(curr);
+            curr = curr.next;
+        }
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+    }
+}
