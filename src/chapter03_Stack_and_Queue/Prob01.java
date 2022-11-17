4e30b8bcc28975fe62601b93bdb9eca24a5d9d0e
1000zoo <cjswldn99@gmail.com>
2022-11-17 21:17:04 +0900

    prob 3.1 / 배열 한 개로 스택 세 개 구현하기 코드 추가

--- src/chapter03_Stack_and_Queue/Prob01.java
+++ src/chapter03_Stack_and_Queue/Prob01.java
@@ -9,6 +9,35 @@ package chapter03_Stack_and_Queue;
    데이터들을 한 칸씩 뒤로 보내준다. 데이터를 리턴받는 경우 (pop) f 값을 리턴해주고 f-- 를 해준다.
  */
 
-public class Prob01 {
+import java.util.Arrays;
 
+public class Prob01 {
+    private final int length = 10;
+    private final int[] stack = new int[length];
+    private final int[] startIndex= new int[] {0, length / 3, 2 * length / 3};
+    private final int[] maxFinalIndex = new int[] {startIndex[1] - 1, startIndex[2] - 1, length - 1};
+
+    private final int[] currIndex = Arrays.copyOf(startIndex, 3);
+
+    public int size(int ch) {
+        return currIndex[ch] - startIndex[ch];
+    }
+    public boolean push(int ch, int val) {
+        if (currIndex[ch] <= maxFinalIndex[ch]){
+            stack[currIndex[ch]++] = val;
+            return true;
+        }
+        return false;
+    }
+    public int pop(int ch) {
+        int _val = stack[currIndex[ch] - 1];
+        stack[--currIndex[ch]] = 0;
+        return _val;
+    }
+    public int peek(int ch) {
+        return stack[currIndex[ch]];
+    }
+    public boolean isEmpty(int ch) {
+        return size(ch) == 0;
+    }
 }
