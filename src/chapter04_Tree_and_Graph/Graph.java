3441ccdd65145082e81a10414d092e188d8b9e81
1000zoo <cjswldn99@gmail.com>
2023-04-12 22:49:40 +0900

    문제풀이를 위한 Graph 클래스

---
+++ src/chapter04_Tree_and_Graph/Graph.java
@@ -0,0 +1,11 @@
+package chapter04_Tree_and_Graph;
+
+import java.util.*;
+
+public class Graph<T> {
+    public List<Node<T>> nodes;
+
+    public Graph() {
+        nodes = new ArrayList<>();
+    }
+}
